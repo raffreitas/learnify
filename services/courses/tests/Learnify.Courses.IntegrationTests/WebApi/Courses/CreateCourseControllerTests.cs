6c9d380db841b4da5f336008e5aca0748f3fd3ec
Rafael Freitas <raffreitasx@gmail.com>
2025-08-10 11:19:40 -0300

    feat: add integration tests for course creation and setup test environment

---
+++ services/courses/tests/Learnify.Courses.IntegrationTests/WebApi/Courses/CreateCourseControllerTests.cs
@@ -0,0 +1,49 @@
+using System.Net;
+using System.Net.Http.Json;
+
+using Learnify.Courses.Application.Courses.UseCases.CreateCourse;
+using Learnify.Courses.IntegrationTests.Shared;
+
+using Shouldly;
+
+namespace Learnify.Courses.IntegrationTests.WebApi.Courses;
+
+[Trait("IntegrationTests", "WebApi - Courses")]
+public class CreateCourseControllerTests(CustomWebApplicationFactory factory)
+    : BaseIntegrationTest(factory), IAsyncLifetime
+{
+    [Fact(DisplayName = nameof(CreateCourse_ShouldSuccess_WhenRequestIsValid))]
+    public async Task CreateCourse_ShouldSuccess_WhenRequestIsValid()
+    {
+        // Arrange
+        var createCourse = new CreateCourseRequest { Title = Faker.Commerce.ProductName() };
+
+        // Act
+        var httpResponse = await PostAsync("/api/courses/v1", createCourse);
+
+        // Assert
+        httpResponse.EnsureSuccessStatusCode();
+        httpResponse.StatusCode.ShouldBe(HttpStatusCode.Created);
+
+        var response = await httpResponse.Content.ReadFromJsonAsync<CreateCourseResponse>();
+        response.ShouldNotBeNull();
+        response.CourseId.ShouldNotBe(Guid.Empty);
+    }
+
+    [Fact(DisplayName = nameof(CreateCourse_ShouldReturnBadRequest_WhenRequestIsInvalid))]
+    public async Task CreateCourse_ShouldReturnBadRequest_WhenRequestIsInvalid()
+    {
+        // Arrange
+        var createCourse = new CreateCourseRequest { Title = string.Empty };
+
+        // Act
+        var httpResponse = await PostAsync("/api/courses/v1", createCourse);
+
+        // Assert
+        httpResponse.IsSuccessStatusCode.ShouldBeFalse();
+        httpResponse.StatusCode.ShouldBe(HttpStatusCode.BadRequest);
+    }
+
+    public Task InitializeAsync() => Task.CompletedTask;
+    public async Task DisposeAsync() => await CleanUpDatabaseAsync();
+}
