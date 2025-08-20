dc0665212e4879c6194451aec9b7b91eb6ad80cd
Rafael Freitas <raffreitasx@gmail.com>
2025-08-19 21:16:14 -0300

    feat: remove identity

--- services/identity/src/Learnify.Identity.WebApi/Features/Users/Domain/Repositories/IUserRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Learnify.Identity.WebApi.Features.Users.Domain.Repositories;
-
-public interface IUserRepository
-{
-    Task AddAsync(User user, CancellationToken cancellationToken = default);
-    Task<User?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
-    Task<User?> GetByProviderKeyAsync(string providerKey, CancellationToken cancellationToken = default);
-}
--- services/identity/src/Learnify.Identity.WebApi/Shared/Infrastructure/Persistence/Factory/IDbConnectionFactory.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Data;
-
-namespace Learnify.Identity.WebApi.Shared.Infrastructure.Persistence.Factory;
-
-public interface IDbConnectionFactory
-{
-    Task<IDbConnection> CreateConnection(CancellationToken cancellationToken = default);
-}
