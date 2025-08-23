dc064be06f917e137020af5817eb5d9dcaeba781
Rafael Freitas <raffreitasx@gmail.com>
2025-08-23 15:30:40 -0300

    feat: update video duration property type to long and optimize lesson title uniqueness check

---
+++ services/video-processing/src/Learnify.VideoProcessing.Infrastructure.Persistence/Migrations/20250823182633_v002.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Learnify.VideoProcessing.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class v002 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "duration",
+                table: "videos",
+                type: "bigint",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double precision",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "duration",
+                table: "videos",
+                type: "double precision",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldNullable: true);
+        }
+    }
+}
